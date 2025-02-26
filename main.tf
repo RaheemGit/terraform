bfb44e69b00f9d44fdab6acfb5a497e7770f3872
RaheemGit <raheem.a1208@gmail.com>
2025-02-26 19:03:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 provider "google" {
   project = "mydemo-452010"  # Replace with your GCP Project ID
-  region  = "us-central1"      # Change to your preferred region
+  region  = "us-central1"     # Change to your preferred region
 }
 
-resource "google_compute_instance" "debian_vm" {
-  name         = "debian-12-vm"
+variable "vm_count" {
+  description = "Number of VMs to create"
+  default     = 3  # Adjust the number of instances as needed
+}
+
+resource "google_compute_instance" "debian_vms" {
+  count        = var.vm_count
+  name         = "debian-12-vm-${count.index}"
   machine_type = "e2-medium" # Choose your preferred machine type
   zone         = "us-central1-a" # Change as needed
 
@@ -21,4 +27,4 @@ resource "google_compute_instance" "debian_vm" {
       # Assigns an external IP
     }
   }
-}  # <-- Missing closing brace added here
+}
